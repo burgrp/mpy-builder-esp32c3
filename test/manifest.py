5db0c94ca87bdf7e607ab5b5eb4e648d367b153a
Pavel Burgr <pb@drake.cz>
2023-01-02 18:49:45 +0100

    WIP

--- test/manifest.py
+++ test/manifest.py
@@ -1,27 +1,29 @@
 import os
 
-### Freeze application files
+
+def freeze(dir, modules):
+    print(dir, modules)
+
+# Freeze application files
+
 
 modules = []
 
-for file in os.listdir("."):
-    if file.endswith(".py") and file != "manifest.py":
+for file in os.listdir('.'):
+    if file.endswith('.py') and file != 'manifest.py':
         modules.append(file)
 
-freeze(".", modules)
+freeze('.', modules)
 
 
-### Freeze external dependencies from requirements directory
+# Freeze external packages
 
-REQ_DIR="requirements"
+PACKAGES_DIR = 'packages'
 
 modules = []
 
-for package in os.listdir(REQ_DIR):
-    if package != "test":
-        for file in os.listdir(REQ_DIR + "/" + package):
-            if file.endswith(".py"):
-                modules.append(package + "/" + file)
-
-freeze(REQ_DIR, modules)
-
+for root, dirs, files in os.walk(PACKAGES_DIR):
+    if '__init__.py' in files:
+        root_parts = root.split('/')
+        d = root_parts[-1:][0]
+        freeze('/' + '/'.join(root_parts[:-1]), [d + "/" + f for f in files])
