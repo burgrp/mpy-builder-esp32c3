ea0d3909e00f63e22e6da9c956f65ab781793be1
Pavel Burgr <pb@drake.cz>
2023-01-03 12:40:56 +0100

    WIP

--- test/app/main.py
+++ test/app/main.py
@@ -1,8 +1,7 @@
-import vec.distance
 
 print("Hello!")
 
+import vec.distance
 a = [2,3]
 b = [5,7]
-
 print("Distance of", a, "and", b, "is", vec.distance.manhattan(a,b))
--- test/manifest.py
+++ test/manifest.py
@@ -1,26 +1,15 @@
 import os
+print("-------------------------------------------------------------------------------");
+freeze("app")
+print("-------------------------------------------------------------------------------");
 
-#####################################################
-# Freeze application files
+# def freeze(d, m):
+#     print(d, m)
 
-modules = []
+# #####################################################
+# # Freeze application files
 
-for file in os.listdir('.'):
-    if file.endswith('.py') and file != 'manifest.py':
-        modules.append(file)
+# modules = []
 
-freeze('.', modules)
-
-
-#####################################################
-# Freeze external packages
-
-PACKAGES_DIR = 'packages'
-
-modules = []
-
-for root, dirs, files in os.walk(PACKAGES_DIR):
-    if '__init__.py' in files:
-        root_parts = root.split('/')
-        d = root_parts[-1:][0]
-        freeze('/'.join(root_parts[:-1]), [d + "/" + f for f in files])
+# for root, dirs, files in os.walk('app'):
+#     print(root, dirs, files)
